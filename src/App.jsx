8274847568a3a20941e16d20321dcc75bd70b053
nathan-au <im.nathanau@gmail.com>
2026-02-03 17:56:45 -0500

    qwe

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import NavBar from "./components/Navbar"
 function App() {
     return (
         <>
-            <NavBar></NavBar>
-            <h1 className="text-5xl text-center">Welcome to Smart Course Companion</h1>
+            <div className="hero bg-base-200 min-h-screen">
+                <NavBar></NavBar>
+                <div className="hero-content text-center flex flex-col">
+                    <h1 className="text-5xl text-center">Welcome to Smart Course Companion</h1>
+                    <p>Developed by Nathan Au, Duval Kwali, and Rami Madadi for SOEN 287</p>
+                </div>
+            </div>
         </>
 
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,21 @@ import { Link } from 'react-router-dom';
 function NavBar() {
   return (
     <>
-        <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar bg-base-100 shadow-sm absolute top-0">
             <div className="navbar-start">
                 <a className="btn btn-ghost text-xl">SCC</a>
             </div>
             <div className="navbar-center">
                 <ul className="menu menu-horizontal px-1">
-                    
-                    <li><Link to="/signin">Sign In</Link></li>
-                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><a>Admins</a></li>
+                    <li><a>Students</a></li>
                 </ul>
             </div>
             <div className="navbar-end">
-                <input type="checkbox" value="dark" className="toggle theme-controller" />
+                <Link className='btn' to="/signin">Sign In</Link>
+                <Link className='btn' to="/signup">Sign Up</Link>
             </div>
         </div>
-       
     </>
   )
 }
