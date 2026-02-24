5ed73496343eb585d6310954204bcc2828e37014
Rami Hamza Madadi <Ramisami@outlook.com>
2026-02-24 17:00:56 -0500

    delete button

--- src/pages/course/CourseDashboard.jsx
+++ src/pages/course/CourseDashboard.jsx
@@ -1,57 +1,113 @@
+
 function CourseDashboard(){
     return(
         <>
-        <ul class="list bg-base-100 rounded-box shadow-md">
+        
+        <ul className="list bg-base-100 rounded-box shadow-md">
   
-  <li class="p-4 pb-2 text-xs opacity-60 tracking-wide">Weekly table</li>
+  <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Weekly table</li>
   
-  <li class="list-row">
-    <div class="text-4xl font-thin opacity-30 tabular-nums">01</div>
+  <li className="list-row">
+    <div className="text-4xl font-thin opacity-30 tabular-nums">01</div>
     <input type="checkbox" defaultChecked className="checkbox checkbox-md" />
-     <div><img class="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
-    <div class="list-col-grow">
+     <div><img className="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
+    <div className="list-col-grow">
       <div>Assignment 0</div>
       
     </div>
-    <button class ="btn btn-square btn-ghost">
- <img class ="w-8.5" src ="https://img.icons8.com/?size=100&id=83268&format=png&color=000000" alt= "triple-dot"/>
-    </button>
-    <button class="btn btn-square btn-ghost">
-      <img class = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000"/>
+    <div className="dropdown dropdown-end">
+  <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
+    <img
+      className="w-8 h-8"
+      src="https://img.icons8.com/?size=100&id=83268&format=png&color=000000"
+      alt="triple-dot"
+    />
+  </div>
+
+  <ul
+    tabIndex={0}
+    className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow"
+  >
+    <li>
+      <button
+        className="text-red-600"
+        onClick={() => console.log("remove assignment")}
+      >
+        Remove assignment
+      </button>
+    </li>
+  </ul>
+</div>
+    <button className="btn btn-square btn-ghost">
+      <img className = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000"/>
       
     </button>
   </li>
   
-  <li class="list-row">
-    <div class="text-4xl font-thin opacity-30 tabular-nums">02</div>
+  <li className="list-row">
+    <div className="text-4xl font-thin opacity-30 tabular-nums">02</div>
     <input type="checkbox" defaultChecked className="checkbox checkbox-md" />
-     <div><img class="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
-    <div class="list-col-grow">
+     <div><img className="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
+    <div className="list-col-grow">
       <div>Lab 0</div>
    
     </div>
-    <button class ="btn btn-square btn-ghost">
- <img class ="w-8.5" src ="https://img.icons8.com/?size=100&id=83268&format=png&color=000000" alt= "triple-dot"/>
-    </button>
-    <button class="btn btn-square btn-ghost">
-      <img class = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000"/>
+    <div className="dropdown dropdown-end">
+  <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
+    <img
+      className="w-8 h-8"
+      src="https://img.icons8.com/?size=100&id=83268&format=png&color=000000"
+      alt="triple-dot"
+    />
+  </div>
+
+  <ul
+    tabIndex={0}
+    className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow"
+  >
+    <li>
+      <button className="text-red-600" onClick={() => console.log("remove Lab 0")}>
+        Remove assignment
+      </button>
+    </li>
+  </ul>
+</div>
+    <button className="btn btn-square btn-ghost">
+      <img className = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000"/>
     </button>
   </li>
   
-  <li class="list-row">
-    <div class="text-4xl font-thin opacity-30 tabular-nums">03</div>
+  <li className="list-row">
+    <div className="text-4xl font-thin opacity-30 tabular-nums">03</div>
     <input type="checkbox" defaultChecked className="checkbox checkbox-md" />
-    <div><img class="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
-    <div class="list-col-grow">
+    <div><img className="size-10 rounded-box" src="https://moodle.concordia.ca/moodle/theme/image.php/saimaniq/assign/1769581239/monologo?filtericon=1"/></div>
+    <div className="list-col-grow">
       <div>Group assignment</div>
-      <div class="text-xs uppercase font-semibold opacity-60">Requires minimum team of 2</div>
+      <div className="text-xs uppercase font-semibold opacity-60">Requires minimum team of 2</div>
     </div>
-    <button class ="btn btn-square btn-ghost">
- <img class ="w-8.5" src ="https://img.icons8.com/?size=100&id=83268&format=png&color=000000" alt= "triple-dot"/>
-    </button>
+    <div className="dropdown dropdown-end">
+  <div tabIndex={0} role="button" className="btn btn-square btn-ghost">
+    <img
+      className="w-8 h-8"
+      src="https://img.icons8.com/?size=100&id=83268&format=png&color=000000"
+      alt="triple-dot"
+    />
+  </div>
+
+  <ul
+    tabIndex={0}
+    className="dropdown-content menu bg-base-100 rounded-box z-10 w-52 p-2 shadow"
+  >
+    <li>
+      <button className="text-red-600" onClick={() => console.log("remove Group assignment")}>
+        Remove assignment
+      </button>
+    </li>
+  </ul>
+</div>
    
-    <button class="btn btn-square btn-ghost">
-      <img class = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000" alt= "Access"/>
+    <button className="btn btn-square btn-ghost">
+      <img className = "w-8.5" src="https://img.icons8.com/?size=100&id=100135&format=png&color=000000" alt= "Access"/>
     </button>
   </li>
   
